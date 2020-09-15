3f8b2f37cbc67012745a4f62a9e87d97a1976714
Pratik Dhakal <dhakalpratik96@gmail.com>
2020-09-15 07:13:21 -0500

    Make it look better

--- P03/full_credit/main.cpp
+++ P03/full_credit/main.cpp
@@ -18,13 +18,14 @@ int main()
     while(!b1.solved())
     {
         std:: string s1 = b1.to_string();
-        std::cout<<"Guess "<< guess <<" select 2 unmatched tiles or enter 0 0 to quit: "<<std::endl;
+        std::cout<<"Guess "<< guess <<" select 2 unmatched tiles : "<<std::endl;
         std::cout<<s1;
         std::cin>> t1 >> t2;
 
         std::cout<<(b1.attempt(t1,t2));
 
         std::cout<<"\n";
+        guess++;
 
     };
 
